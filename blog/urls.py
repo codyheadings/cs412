1a0fc0d068d809f3f45f89b26374845319af7a52
Cody Headings <codyh@bu.edu>
2025-09-25 18:06:39 -0400

    Finished blog example.

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 # blog/urls.py
 from django.urls import path
-from .views import ShowAllView 
+from .views import ArticleView, ShowAllView, RandomArticleView
 
 # our view class definition
 urlpatterns = [
     # map the URL (empty string) to the view
-    path('', ShowAllView.as_view(), name='show_all'),
+    path('', RandomArticleView.as_view(), name='random'),
+    path('show_all', ShowAllView.as_view(), name='blog/show_all'),
+    path('article/<int:pk>', ArticleView.as_view(), name='article'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Article
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+import random
 
 # Create your views here.
 class ShowAllView(ListView):
@@ -9,3 +10,24 @@ class ShowAllView(ListView):
     model = Article # retrieve objects of type Article from the database
     template_name = 'blog/show_all.html'
     context_object_name = 'articles'
+
+class ArticleView(DetailView):
+    """Display a single article"""
+
+    model = Article
+    template_name = "blog/article.html"
+    context_object_name = "article"
+
+class RandomArticleView(DetailView):
+    """Display a single article selected at random."""
+
+    model = Article
+    template_name = "blog/article.html"
+    context_object_name = "article"
+
+    # methods
+    def get_object(self):
+        """return one instance of random article"""
+        all_articles = Article.objects.all()
+        article = random.choice(all_articles)
+        return article
