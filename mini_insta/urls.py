718fb432a5a2611e6cfdaf5e8ab020511b1e77e8
Cody Headings <codyh@bu.edu>
2025-09-25 20:21:19 -0400

    Added all files for deployment.

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -3,10 +3,9 @@
 # desc: url pattern routes for app navigation
 
 from django.urls import path
-from .views import ProfileListView
+from .views import ProfileDetailView, ProfileListView
 
 urlpatterns = [
-    # path('', RandomArticleView.as_view(), name='random'),
     path('', ProfileListView.as_view(), name='show_all_profiles'),
-    # path('profile/<int:pk>', ProfileView.as_view(), name='profile'),
+    path('profile/<int:pk>', ProfileDetailView.as_view(), name='show_profile'),
 ]
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -14,3 +14,10 @@ class ProfileListView(ListView):
     model = Profile
     template_name = 'mini_insta/show_all_profiles.html'
     context_object_name = 'profiles'
+
+class ProfileDetailView(DetailView):
+    """Subclass of DetailView to display a single profile page."""
+    
+    model = Profile
+    template_name = 'mini_insta/show_profile.html'
+    context_object_name = 'profile'
