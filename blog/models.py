b4261e3fdc211e9e2f6d9bd17c7fe50ed0e98cc9
Cody Headings <codyh@bu.edu>
2025-09-30 11:25:22 -0400

    Prelecture examples for 9/30: adding comments and new articles

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 # blog/models.py
 # Define the data objects for our application
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Article(models.Model):
@@ -13,3 +14,25 @@ class Article(models.Model):
     def __str__(self):
         '''Return a string representation of this Article object.'''
         return f'{self.title} by {self.author}'
+    
+    def get_absolute_url(self):
+        """Return a URl to display one instance of this model."""
+        return reverse('article', kwargs={'pk': self.pk})
+    
+    def get_all_comments(self):
+        """Return a QuerySet of comments abour this article."""
+        comments = Comment.objects.filter(article=self)
+        return comments
+    
+class Comment(models.Model):
+    """Encapsulate the idea of a comment on an article."""
+
+    # data attributs for the Comment:
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author = models.TextField(blank=False)
+    text = models.TextField(blank=False)
+    published = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return a string representation of this Comment."""
+        return f'{self.text}'
--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 # blog/urls.py
 from django.urls import path
-from .views import ArticleView, ShowAllView, RandomArticleView
+from .views import *
 
 # our view class definition
 urlpatterns = [
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', RandomArticleView.as_view(), name='random'),
     path('show_all', ShowAllView.as_view(), name='blog/show_all'),
     path('article/<int:pk>', ArticleView.as_view(), name='article'),
+    path('article/create', CreateArticleView.as_view(), name='create_article'),
+    path('article/<int:pk>/create_comment', CreateCommentView.as_view(), name='create_comment'),
 ]
