e1556c50359a58d8dda5598b6346fcc598dd92f2
Cody Headings <codyh@bu.edu>
2025-11-14 15:48:02 -0500

    Trying a different way to fix CORS

--- cs412/settings.py
+++ cs412/settings.py
@@ -64,6 +64,11 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8081",
+    "http://localhost:3000",
+]
+
 ROOT_URLCONF = 'cs412.urls'
 
 TEMPLATES = [
