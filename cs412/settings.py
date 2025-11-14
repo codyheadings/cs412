3c2cf8cb361c4855d1fa8a4b2896990d221ad944
Cody Headings <codyh@bu.edu>
2025-11-14 15:39:35 -0500

    Added CORS to prevent API calls being blocked

--- cs412/settings.py
+++ cs412/settings.py
@@ -48,9 +48,11 @@ INSTALLED_APPS = [
     'voter_analytics',
     "rest_framework", # NEW
     "dadjokes",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -59,6 +61,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'cs412.urls'
 
